04479eac1b5b323888abe7c2f75fd9e60ee7389b
Marko Mutavdzic <belikompijuter@gmail.com>
2023-11-18 20:48:46 +0100

    Set launchScreen

---
+++ RealityFace/Cells/MenuCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuCell.swift
+//  RealityFace
+//
+//  Created by Marko on 18.11.23..
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func addImage() {
+        
+    }
+
+}
