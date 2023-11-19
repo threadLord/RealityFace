66e9931b767077d2f842b588c5de7c8b4faa747b
Marko Mutavdzic <belikompijuter@gmail.com>
2023-11-19 14:16:26 +0100

    Remove dead code

--- RealityFace/Content/Mask.swift
+++ RealityFace/Content/Mask.swift
@@ -32,14 +32,3 @@ class Mask: SCNNode, Content {
         faceGeometry.update(from: anchor.geometry)
     }
 }
-
-
-extension UIColor {
-    func imageWithColor(width: Int, height: Int) -> UIImage {
-        let size = CGSize(width: width, height: height)
-        return UIGraphicsImageRenderer(size: size).image { rendererContext in
-            self.setFill()
-            rendererContext.fill(CGRect(origin: .zero, size: size))
-        }
-    }
-}
